79bcf8ff2bfac0a4b37a86f50d5d3a794b389b21
Emircan  Yaşar <yasar.emircann@gmail.com>
2023-08-12 15:30:22 +0300

    first commit

---
+++ components/UI & Layout/Logo.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Logo = () => {
+  return (
+    <div className='450:w-[120px] max-450:h-[50px] z-10'>
+        <Link href='/'>
+            <Image alt='' src='/images/logo.png' width={500} height={500} className='w-full h-full object-contain'/>
+        </Link>
+        </div>
+  )
+}
+
+export default Logo
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,31 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        '1336': '1337px',
+        '1280': '1281px',
+        '1140': '1141px',
+        '768': '769px',
+        '450': '450px',
+      },
+      colors : {
+        'primary': '#050227',
+        'primary-light': '#0b0737',
+        'primary-dark': '#09071d',
+        'primary-lighter': '#120e47',
+        'secondary': '#8585f5',
+        'secondary-light': '#b6b6f8',
+        'graident': 'rgb(244 63 94) ',
+        'graident-dark': '#eb5f5f ',
+        'success': '#6DD432 ',
+        'button': '#1d1a5b  ',
+        'section': '#161550',
+        'star': '#FFCC00',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+  ]
 }
